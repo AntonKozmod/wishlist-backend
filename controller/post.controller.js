512c2a79e4431a710dc1f68b71e52406d9e7b315
Kozmod <antoxa.kozmod@gmail.com>
2024-01-05 23:30:16 +0300

    fixes for crud posts

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -1,19 +1,27 @@
+const db = require('../db');
+
 class PostController {
-    async getPosts(_, res) {
+    getUserPosts = async (req, res) => {
+        const { user_id } = req.query;
+        if (!user_id) {
+            this.getFriendsPosts(req, res);
+            return;
+        }
+        const posts = await db.query('SELECT * FROM post WHERE person_id = $1', [user_id]);
+        res.json(posts?.rows ?? []);
+    };
+    getFriendsPosts = async (req, res) => {
+        console.log('getFriendsPosts', req);
+        // todo Реализовать получение постов друзей
         const posts = await db.query('SELECT * FROM post');
         res.json(posts?.rows ?? []);
-    }
-    async getOnePost(req, res) {
+    };
+    getOnePost = async (req, res) => {
         const { id } = req.params;
         const posts = await db.query('SELECT * FROM post WHERE id = $1', [id]);
         res.json(posts?.rows ?? []);
-    }
-    async getUserPosts(req, res) {
-        const { user_id } = req.params;
-        const posts = await db.query('SELECT * FROM post WHERE person_id = $1', [user_id]);
-        res.json(posts?.rows ?? []);
-    }
-    async createPost(req, res) {
+    };
+    createPost = async (req, res) => {
         const { user_id, title, content } = req.body;
         const newPost = await db.query('INSERT INTO post (person_id, title, content) VALUES ($1, $2, $3) RETURNING *', [
             user_id,
@@ -21,17 +29,17 @@ class PostController {
             content
         ]);
         res.json(newPost?.rows?.[0]);
-    }
-    async updatePost(req, res) {
+    };
+    updatePost = async (req, res) => {
         const { id, title, content } = req.body;
         const posts = await db.query('UPDATE post SET title = $1, content = $2 WHERE id = $3', [title, content, id]);
         res.json(posts?.rows?.[0]);
-    }
-    async deletePost(req, res) {
+    };
+    deletePost = async (req, res) => {
         const { id } = req.params;
         const posts = await db.query('DELETE FROM post WHERE id = $1', [id]);
         res.json(!!posts?.rowCount);
-    }
+    };
 }
 
 module.exports = new PostController();
--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,9 +1,8 @@
 module.exports = router => {
     const postController = require('../controller/post.controller');
 
-    router.get('/post', postController.getPosts);
+    router.get('/post', postController.getUserPosts);
     router.get('/post/:id', postController.getOnePost);
-    router.get('/post/:user_id', postController.getUserPosts);
     router.post('/post', postController.createPost);
     router.put('/post', postController.updatePost);
     router.delete('/post/:id', postController.deletePost);
