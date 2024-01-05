866bf1cc43811ccebf0893571b39a9b6cddc2df0
Kozmod <antoxa.kozmod@gmail.com>
2024-01-05 22:27:44 +0300

    base crud users

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,18 +1,35 @@
+const db = require('../db');
+
 class UserController {
     async createUser(req, res) {
-        res.send('createUser')
+        const { name, surname, password, email, is_admin } = req.body;
+        const newPerson = await db.query('INSERT INTO person (name, surname, password, email, is_admin) VALUES ($1, $2, $3, $4, $5) RETURNING *', [name, surname, password, email, is_admin]);
+        res.json(newPerson?.rows?.[0]);
     }
-    async getUsers(req, res) {
-        res.send('getUsers')
+    async getUsers(_, res) {
+        const persons = await db.query('SELECT * FROM person');
+        res.json(persons?.rows ?? []);
     }
     async getOneUser(req, res) {
-        res.send('getOneUser')
+        const { id } = req.params;
+        const users = await db.query('SELECT * FROM person WHERE id = $1', [id]);
+        res.json(users?.rows?.[0]);
+    }
+    async getUserByPassword(req, res) {
+        const { email, password } = req.body;
+        const users = await db.query('SELECT * FROM person WHERE email = $1 AND password = $2', [email, password]);
+        res.json(users?.rows?.[0]);
     }
     async updateUser(req, res) {
-        res.send('updateUser')
+        const { id, name, surname, password, email, is_admin } = req.body;
+        const users = await db.query('UPDATE person SET name = $1, surname = $2, password = $3, email = $4, is_admin = $5 WHERE id = $6', [name, surname, password, email, is_admin, id]);
+        res.json(users?.rows?.[0]);
     }
     async deleteUser(req, res) {
-        res.send('deleteUser')
+        const { id } = req.params;
+        const users = await db.query('DELETE FROM person WHERE id = $1', [id]);
+        res.json(!!users?.rowCount);
+
     }
 }
 
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
